67e0e6463973bdfe47fbe7213716bc731730b398
SDM30 <simondiaz@javeriana.edu.co>
2026-02-08 13:15:28 -0500

    editor en angular envoltorio de code mirror

---
+++ frontend/src/app/editor/editor.ts
@@ -0,0 +1,45 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CodeEditor } from '@acrodata/code-editor';
+import { Extension } from '@codemirror/state';
+import { cpp } from '@codemirror/lang-cpp';
+
+// Imports de los temas que mencionaste
+import { oneDark } from '@codemirror/theme-one-dark';
+import { dracula } from '@uiw/codemirror-theme-dracula';
+import { solarizedLight, solarizedDark } from '@uiw/codemirror-theme-solarized';
+import { nord } from '@uiw/codemirror-theme-nord';
+import { kimbie } from '@uiw/codemirror-theme-kimbie';
+import { languages as cmLanguages } from '@codemirror/language-data';
+import type { LanguageDescription } from '@codemirror/language';
+
+export type Theme = 'light' | 'dark' | Extension;
+
+@Component({
+  selector: 'app-editor',
+  standalone: true,
+  imports: [FormsModule, CodeEditor],
+  templateUrl: './editor.html',
+  styleUrl: './editor.css',
+})
+export class Editor {
+  value = `#include <iostream>\n\nint main() {\n    std::cout << "Hola C++" << std::endl;\n    return 0;\n}`;
+
+  /** Opcion por defecto */
+  @Input() theme: Theme = 'light';
+
+  /** Opciones para los temas del editor */
+  themeOptions = [
+    { label: 'Standard Light', value: 'light' },
+    { label: 'Standard Dark', value: 'dark' },
+    { label: 'One Dark', value: oneDark },
+    { label: 'Dracula', value: dracula },
+    { label: 'Solarized Light', value: solarizedLight },
+    { label: 'Solarized Dark', value: solarizedDark },
+    { label: 'Nord', value: nord },
+    { label: 'Kimbie', value: kimbie },
+  ];
+
+  languages: LanguageDescription[] = cmLanguages;
+  language = 'cpp';
+}
