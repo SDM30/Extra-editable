2b9a3544a6a594a38b63b2c248fbb349378b3d1b
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2026-04-27 20:43:33 -0500

    Delete Spring Backend

--- backend/src/main/java/com/app/backend/repository/EjercicioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.backend.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.backend.model.Ejercicio;
-
-public interface EjercicioRepository extends JpaRepository<Ejercicio, Long> {
-}
