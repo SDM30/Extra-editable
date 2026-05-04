118a58c0b2f8e246fe9ae8eee87bb4d26fe0d4ed
SamuOsorio <osorio.samuel@gmail.com>
2026-05-03 22:06:38 -0500

    Feat: Se incorporo el api gateway para el LSP
    - El proxy en FastAPI a LSP corre en el puerto 8135
    - Por el puerto 8080 se accede al LSP desde el gateway

--- frontend/src/app/environments/enviroment.ts
+++ frontend/src/app/environments/enviroment.ts
@@ -2,5 +2,5 @@ export const enviroment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
   // Se accede al Language Service a través del gateway (Nginx) en `/lsp/`
-  apiUrlLanguageServer: 'http://localhost:8135',
+  apiUrlLanguageServer: 'http://localhost:8080',
 };
